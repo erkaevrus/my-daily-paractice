23764d90d15012807d55628324e4744a955844e1
erkaevrus <erk_08@mail.ru>
2023-01-03 08:57:15 +0500

    add tasks' solving 4,5

--- kata_every_day.js
+++ kata_every_day.js
@@ -5,6 +5,7 @@
 //               "1" -->                "1"
 //                "" -->                 ""*/
 
+
 function maskify(line) {
   if (line.length <= 4) {
   return line
@@ -19,6 +20,7 @@ function maskify(line) {
   return res.reverse().join('')
 }
 
+
 /*#2 Simple, given a string of words, return the length of the shortest word(s).
 String will never be empty and you do not need to account for different data types. */
 
@@ -26,8 +28,26 @@ function findShort(s){
   return Math.min(...(s.split(' ').map((x) => x.length)))
 }
 
+
 /*#3 In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out. */
 
 function filter_list(l) {
   return l.filter((x) => typeof(x) !== 'string')
+}
+
+
+
+/*#4 In this kata, you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. */
+
+function squareDigits(num){
+  return +(String(num).split('').map(x => x**2).join(''))
+}
+
+/*#5 Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"] */
+
+function friend(friends){
+  return friends.filter(x => x.length === 4 && /[a-zA-Z]/.test(x))
 }
