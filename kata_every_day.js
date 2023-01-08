4e86aa27f4fdafdc6eb884457e4a1c883b1154c8
erkaevrus <erk_08@mail.ru>
2023-01-08 15:50:56 +0500

    add task's solution 12

--- kata_every_day.js
+++ kata_every_day.js
@@ -152,3 +152,25 @@ function flatten(...args) {
   }
   return result
 }
+
+
+/*Your task is to implement function toReadable that converts given number, to readable string.
+toReadable(1); // Will return 'one'
+toReadable(997); //will return 'nine hundred ninety seven'*/
+function toReadable (number) {
+  if (number === 0) {return 'zero'}
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  if (number < 20){
+    result = units[number]
+  } else if (number < 100) {
+    result = `${tens[Math.trunc(number / 10)]} ${units[number % 10]}`
+  } else {
+    if (number % 100 < 20) {
+      result = `${units[Math.trunc(number / 100 )]} hundred ${units[number % 100]}`
+    } else {
+      result = `${units[Math.trunc(number / 100 )]} hundred ${tens[Math.trunc(number % 100 / 10)]} ${units[number % 10]}`
+    }
+  }
+  return result.trimEnd()
+}
