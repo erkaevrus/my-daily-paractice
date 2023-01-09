c5f0831ddaffd56e7a779b140cda15e29ff79da6
erkaevrus <erk_08@mail.ru>
2023-01-09 08:03:24 +0500

    add task's solution 13

--- kata_every_day.js
+++ kata_every_day.js
@@ -174,3 +174,82 @@ function toReadable (number) {
   }
   return result.trimEnd()
 }
+
+
+/*13 Your task is to write a function, that decodes Morse code and returns a string. Write your solution in src/index.js
+Input: String. Its length is multiple of 10.
+Each letter from alphabet encoded with dots(.) and dashes(-). 10 stands for dot(.), 11 stands for dash(-).
+Each encoded letter's length is 10.
+If the length of the encoded letter is less then 10, it left padded with 0.
+Space in string is **********. Output: String (decoded) Example: me -> m === -- === 0000001111, e === . === 0000000010 -> 00000011110000000010*/
+const MORSE_TABLE = {
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
+};
+
+function decode(expr) {
+  const arrDigitWord = expr.split('**********')
+  const arrDigitAlpha = []
+  let line = []
+  let letter = ''
+  let result = ''
+
+  for (let item of arrDigitWord) {
+      for (let i = 0; i < item.length; i += 10) {
+      line.push(item.slice(i, i+10))
+      }
+        arrDigitAlpha.push(line)
+        line = []
+  }
+
+  for (let item of arrDigitAlpha) {
+    for (let str of item) {
+        for (let i = 0; i < str.length; i +=2) {
+            if (str.slice(i, i + 2) === '10') {
+                letter += '.'
+            }
+            if (str.slice(i, i + 2) === '11') {
+                letter += '-'
+            }
+        }
+        result += MORSE_TABLE[letter]
+        letter = ''
+    }
+    result += ' '
+  }
+  return result.trim()
+}
