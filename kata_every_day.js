ccd858d49c4bdba28dd7618ef95e3711e1f6f823
erkaevrus <erk_08@mail.ru>
2023-01-10 13:08:06 +0500

    add tasks' solutions 16,17

--- kata_every_day.js
+++ kata_every_day.js
@@ -293,3 +293,64 @@ function mapCycle(arr, cb) {
   }
   return res
 }
+
+// #16 Реализация метода map с помощью Рекурсии
+function mapRecursion(){
+  const res = []
+  let i = 0
+
+  function inner(arr, cb) {
+    if (i >= arr.length) {
+      return res
+  }
+    res.push(cb(arr[i]))
+    i++
+    return inner(arr, cb)
+  }
+  return inner
+}
+
+
+/*#17 Math geeks and computer nerds love to anthropomorphize numbers and assign emotions and personalities to them. Thus there is defined the concept of a "happy" number. A happy number is defined as an integer in which the following sequence ends with the number 1.
+- Start with the number itself.
+- Calculate the sum of the square of each individual digit.
+- If the sum is equal to 1, then the number is happy. If the sum is not equal to 1, then repeat steps 1 and 2. A number is considered unhappy once the same number occurs multiple times in a sequence because this means there is a loop and it will never reach 1.
+
+For example, the number 7 is a "happy" number: 72 = 49 --> 42 + 92 = 97 --> 92 + 72 = 130 --> 12 + 32 + 02 = 10 --> 12 + 02 = 1
+Once the sequence reaches the number 1, it will stay there forever since 12 = 1
+On the other hand, the number 6 is not a happy number as the sequence that is generated is the following: 6, 36, 45, 41, 17, 50, 25, 29, 85, 89, 145, 42, 20, 4, 16, 37, 58, 89
+Once the same number occurs twice in the sequence, the sequence is guaranteed to go on infinitely, never hitting the number 1, since it repeat this cycle.
+Your task is to write a program which will print a list of all happy numbers between 1 and x (both inclusive), where:
+2 <= x <= 10000*/
+function happyNumbers(x){
+  const result = []
+  let store = []
+  let line
+  let sumSquare = 0
+
+  for (let current = 1; current <= x; current++) {
+    line = String(current).split('')
+    while (true) {
+      for (let j = 0; j < line.length; j++) {
+        sumSquare += Math.pow(+(line[j]), 2)
+        console.log(sumSquare)
+      }
+      if (sumSquare === 1) {
+        result.push(current)
+        store = []
+        sumSquare = 0
+        break
+      }
+      if (store.includes(sumSquare)) {
+        store = []
+        sumSquare = 0
+        break
+      } else {
+        store.push(sumSquare)
+        line = String(sumSquare).split('')
+        sumSquare = 0
+      }
+    }
+  }
+  return result
+}
