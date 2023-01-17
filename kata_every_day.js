91d62c87205dd89ee57ff37a56dfdd36bc682379
erkaevrus <erk_08@mail.ru>
2023-01-17 12:46:21 +0500

    add task's solution 24

--- kata_every_day.js
+++ kata_every_day.js
@@ -468,3 +468,25 @@ function combine(...args){
   }
   return result
 }
+
+
+/*#24 Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+Square all numbers k (0 <= k <= n) between 0 and n.
+Count the numbers of digits d used in the writing of all the k**2.
+Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+Examples:
+n = 10, d = 1
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.*/
+function nbDig(n, d) {
+  let count = 0
+  const temp = Array.from(Array(n + 1).keys()).map(x => String(x**2))
+  for (let item of temp) {
+    for (let digit of item) {
+      if (digit === String(d)) {
+        count++
+      }
+    }
+  }
+  return count
+}
