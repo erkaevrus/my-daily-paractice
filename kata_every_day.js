7f161e5ddb8ba68ca3c937ba11b2c4c941bc78a7
erkaevrus <erk_08@mail.ru>
2023-01-20 13:07:59 +0500

    add task's solution 27

--- kata_every_day.js
+++ kata_every_day.js
@@ -531,3 +531,51 @@ function maxTriSum(numbers){
   }
   return temp.reduce((sum, current) => sum + current, 0)
 }
+
+
+/*20.01.2023 #27 Написать функцию которая печатает матрицу размером n, которая закручивается по спирали!
+[
+  [ 1,  2,  3,  4,  5 ],
+  [ 16, 17, 18, 19, 6 ],
+  [ 15, 24, 25, 20, 7 ],
+  [ 14, 23, 22, 21, 8 ],
+  [ 13, 12, 11, 10, 9 ]
+]
+*/
+function spiralMatrix(n) {
+  const result = Array.from({length: n}, () => Array(n).fill(''));
+
+  let counter = 1
+  let startCol = 0
+  let endCol = n - 1
+  let startRow = 0
+  let endRow = n -1
+
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let i = startCol; i <= endCol; i++) {
+      result[startRow][i] = counter
+      counter++
+    }
+    startRow++
+
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endCol] = counter
+      counter++
+    }
+    endCol--
+
+    for (let i = endCol; i >= startCol; i--) {
+      result[endRow][i] = counter
+      counter++
+    }
+    endRow--
+
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startCol] = counter
+      counter++
+    }
+    startCol++
+  }
+
+  return result
+}
