61ebfa6884a49526733867fa5558e5601e605df0
erkaevrus <erk_08@mail.ru>
2023-01-23 15:42:20 +0500

    add task's solution 30

--- kata_every_day.js
+++ kata_every_day.js
@@ -635,3 +635,38 @@ function randomMatrix() {
   }
   return result
   }
+
+/*Add a groupBy method to Array.prototype so that elements in an array could be grouped by the result of evaluating a function on each element.
+The method should return an object, in which for each different value returned by the function there is a property whose value is the array of elements that return the same value.
+If no function is passed, the element itself should be taken.
+Example:
+const arr = [1, 2, 3, 2, 4, 1, 5, 1, 6];
+arr.groupBy();
+{
+  1: [1, 1, 1],
+  2: [2, 2],
+  3: [3],
+  4: [4],
+  5: [5],
+  6: [6]
+}
+const arr = [1, 2, 3, 2, 4, 1, 5, 1, 6]
+arr.groupBy(function(val) { return val % 3; })
+{
+  0: [3, 6],
+  1: [1, 4, 1, 1],
+  2: [2, 2, 5]
+*/
+Array.prototype.groupBy = function(cb) {
+  const obj = {};
+  const arr = this;
+  for (let i = 0; i < this.length; i++) {
+    const key = cb ? cb(arr[i]) : arr[i]
+    if (key in obj) {
+      obj[key].push(arr[i])
+    } else {
+      obj[key] = [arr[i]]
+    }
+  }
+return obj
+}
