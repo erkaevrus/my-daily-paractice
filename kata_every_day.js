9e19a8a369f544e321678b8a024a4e50038aa318
erkaevrus <erk_08@mail.ru>
2023-01-27 09:33:02 +0500

    add task's solution 32

--- kata_every_day.js
+++ kata_every_day.js
@@ -683,3 +683,13 @@ function vowelOne(s){
   const vowel = ['a', 'e', 'i', 'o', 'u']
   return s.split('').map(x => vowel.includes(x.toLowerCase()) ? 1 : 0).join('')
 }
+
+
+/*27.01.2023 #32 You must create a function, spread, that takes a function and a list of arguments to be applied to that function. You must make this function return the result of calling the given function/lambda with the given arguments.
+eg:
+spread(someFunction, [1, true, "Foo", "bar"] )
+is the same as...
+someFunction(1, true, "Foo", "bar")*/
+function spread(func, args) {
+  return func(...args);
+}
