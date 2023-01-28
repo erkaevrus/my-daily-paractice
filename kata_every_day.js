c5fdfa8f4605e25496c6290fef405bfcba0e782f
erkaevrus <erk_08@mail.ru>
2023-01-28 19:45:16 +0500

    add task's solution 35

--- kata_every_day.js
+++ kata_every_day.js
@@ -747,3 +747,32 @@ function whoseBicycle(diary1, diary2, diary3) {
   let maxScore = Math.max(sum1, sum2, sum3)
   return `I need to buy a bicycle for my ${maxScore === sum3 ? 'third' : maxScore === sum2 ? 'second' : 'first'} son.`
 }
+
+
+/**28.01.2023 #35 Who is the killer?
+Some people have been killed!
+You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+Task.
+Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+{'James': ['Jacob', 'Bill', 'Lucas'],
+ 'Johnny': ['David', 'Kyle', 'Lucas'],
+ 'Peter': ['Lucy', 'Kyle']}
+and also a list of the names of the dead people:
+['Lucas', 'Bill']
+return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'*/
+function killer(suspectInfo, dead) {
+  let count = 0
+  let killer = ''
+
+  for (let suspect in suspectInfo) {
+    for(let deceased of dead) {
+      if (suspectInfo[suspect].includes(deceased)) {
+        count++
+      }
+      if (count === dead.length) {
+        killer = suspect
+        return killer
+      }
+    }
+  }
+}
