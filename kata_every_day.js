347e4782ca6e695e88205bf502b547f984bd5b4a
erkaevrus <erk_08@mail.ru>
2023-01-28 18:51:08 +0500

    add task's solution 33

--- kata_every_day.js
+++ kata_every_day.js
@@ -693,3 +693,29 @@ someFunction(1, true, "Foo", "bar")*/
 function spread(func, args) {
   return func(...args);
 }
+
+
+/*28.01.2023 #33 A stream of data is received and needs to be reversed.
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+The data is given in an array as such:
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.*/
+function dataReverse(data) {
+  let temp = []
+  let res = []
+  for (let i = data.length - 1; i >= -1; i--) {
+    if (temp.length === 8) {
+      temp.reverse()
+      res.push(...temp)
+      temp = []
+    }
+    temp.push(data[i])
+  }
+return res
+}
