6dfe19e9ff90dd04f0783b93ea02b50f898f6592
erkaevrus <erk_08@mail.ru>
2023-01-30 14:49:18 +0500

    add task's solution 40

--- kata_every_day.js
+++ kata_every_day.js
@@ -875,3 +875,26 @@ function nicknameGenerator(name){
     return name.slice(0, 3);
   }
 }
+
+
+/*30.01.2023 #40 Create a function strCount (takes an object as argument) that will count all string values inside an object. For example:
+strCount({
+  first: "1",
+  second: "2",
+  third: false,
+  fourth: ["anytime",2,3,4],
+  fifth:  null
+  })
+  //returns 3*/
+function strCount(obj){
+  let count = 0
+  for (let item in obj){
+    if(typeof(obj[item]) === 'object'){
+     count += strCount(obj[item])
+    }
+    if (typeof(obj[item]) === 'string'){
+      count++
+    }
+  }
+    return count
+}
