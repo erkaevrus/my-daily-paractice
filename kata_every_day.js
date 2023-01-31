7d50be95b9d2eefe8fe0497f0996c891768fdef3
erkaevrus <erk_08@mail.ru>
2023-01-31 13:58:29 +0500

    add task's solution 41

--- kata_every_day.js
+++ kata_every_day.js
@@ -898,3 +898,13 @@ function strCount(obj){
   }
     return count
 }
+
+
+/*31.01.2023 #41 Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case*/
+const countBits = function (n) {
+  return n.toString(2)
+          .split('')
+          .filter( x => x === '1')
+          .length
+}
