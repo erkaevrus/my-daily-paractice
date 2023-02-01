59aa0f4ca37c820dfed934a050d85bb6f3e755b4
erkaevrus <erk_08@mail.ru>
2023-02-01 12:33:26 +0500

    add task's solution 44

--- kata_every_day.js
+++ kata_every_day.js
@@ -946,3 +946,86 @@ function likes(names) {
       default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
  }
 }
+
+/*01.02.2023 #44 After yet another dispute on their game the Bingo Association decides to change course and automate the game.
+Can you help the association by writing a method to create a random Bingo card?
+Bingo Cards
+A Bingo card contains 24 unique and random numbers according to this scheme:
+5 numbers from the B column in the range 1 to 15
+5 numbers from the I column in the range 16 to 30
+4 numbers from the N column in the range 31 to 45
+5 numbers from the G column in the range 46 to 60
+5 numbers from the O column in the range 61 to 75
+Task
+Write the function get_card()/getCard(). The card must be returned as an array of Bingo style numbers:
+[ 'B14', 'B12', 'B5', 'B6', 'B3', 'I28', 'I27', ... ]
+The numbers must be in the order of their column: B, I, N, G, O. Within the columns the order of the numbers is random.
+a bingo card*/
+function getCard() {
+  const bingoCard = []
+  let num
+
+  for (i = 0; i <= 4; i++) {
+    while(bingoCard.length <= 4) {
+      num = `B${getRandom(1, 15)}`
+      if (bingoCard.includes(num)) {
+        continue
+      } else {
+        bingoCard.push(num)
+      }
+    }
+  }
+
+  for (i = 0; i <= 4; i++) {
+    while(bingoCard.length <= 9) {
+      num = `I${getRandom(16, 30)}`
+      if (bingoCard.includes(num)) {
+        continue
+      } else {
+        bingoCard.push(num)
+      }
+    }
+  }
+
+  for (i = 0; i <= 4; i++) {
+    while(bingoCard.length <= 14) {
+      num = `N${getRandom(31, 45)}`
+      if (bingoCard.includes(num)) {
+        continue
+      } else {
+        bingoCard.push(num)
+      }
+    }
+  }
+
+  for (i = 0; i <= 4; i++) {
+    while(bingoCard.length <= 19) {
+      num = `G${getRandom(46, 60)}`
+      if (bingoCard.includes(num)) {
+        continue
+      } else {
+        bingoCard.push(num)
+      }
+    }
+  }
+
+  for (i = 0; i <= 4; i++) {
+    while(bingoCard.length <= 24) {
+      num = `O${getRandom(61, 75)}`
+      if (bingoCard.includes(num)) {
+        continue
+      } else {
+        bingoCard.push(num)
+      }
+    }
+  }
+
+  bingoCard.splice(12, 1)
+  return bingoCard
+}
+
+
+function getRandom(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
