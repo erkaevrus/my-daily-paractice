e5106a99e63e05d46293e84d1399887b70577f11
erkaevrus <erk_08@mail.ru>
2023-02-06 09:09:13 +0500

    add task's solution 49

--- kata_every_day.js
+++ kata_every_day.js
@@ -1127,3 +1127,21 @@ function toWeirdCase(string){
   }
   return res.trim()
 }
+
+
+/*06.02.2023 The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+The following are examples of expected output values:
+rgb(255, 255, 255) // returns FFFFFF
+rgb(255, 255, 300) // returns FFFFFF
+rgb(0,0,0) // returns 000000
+rgb(148, 0, 211) // returns 9400D3 */
+function rgb(r, g, b){
+  let temp = [r, g, b]
+
+  for (let i = 0; i < temp.length; i++) {
+    temp[i] = temp[i] < 0 ? 0 : temp[i]
+    temp[i] = temp[i] > 255 ? 255 : temp[i]
+  }
+  return temp.map(x => x.toString(16)).map(x => x.length !== 2 ? `0${x}` : x).join('').toUpperCase()
+}
