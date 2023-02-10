68d1acc8b25d5222c9488b14579b03573c7cfe31
erkaevrus <erk_08@mail.ru>
2023-02-10 14:19:48 +0500

    add task's solution 54

--- kata_every_day.js
+++ kata_every_day.js
@@ -1245,3 +1245,16 @@ should return :
 function explode(s) {
   return s.split('').map(x => x.repeat(Number(x))).join('')
 }
+
+
+/*10.02.2023 #53 |CW 6kyu|
+Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+Every year he takes photos of farmers handshaking. Johnny knows that no two farmers handshake more than once. He also knows that some of the possible handshake combinations may not happen.
+However, Johnny would like to know the minimal amount of people that participated this year just by counting all the handshakes.
+Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people needed to perform these handshakes (a pair of farmers handshake only once).
+*/
+function getParticipants(handshakes){
+  if (handshakes === 0) return 0
+  let descriminant = 1 + 8 * handshakes
+  return Math.ceil((1 + Math.sqrt(descriminant))/2)
+}
