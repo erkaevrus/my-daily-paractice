8f9fa975d8cdb25036605d3283ff77cebfbbafd1
erkaevrus <erk_08@mail.ru>
2023-02-13 09:27:17 +0500

    add task's solution 57

--- kata_every_day.js
+++ kata_every_day.js
@@ -1300,3 +1300,36 @@ function toRoman(number) {
        RomansNum[1][Math.floor(number % 100 / 10)] +
        RomansNum[0][Math.floor(number % 10)]
 }
+
+
+/* 13.02.2023 #57 |CW 6kyu|
+I love Fibonacci numbers in general, but I must admit I love some more than others.
+I would like for you to write me a function that, when given a number n (n >= 1 ), returns the nth number in the Fibonacci Sequence.
+For example:
+   nthFibo(4) == 2
+Because 2 is the 4th number in the Fibonacci Sequence.
+For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+*/
+/*
+1. Понимание -> Написать функцию, которая возвращает n-ый член последовательности Фиббоначи. n > 1
+2. Планирование + Декомпозиция
+-> Присвоить переменным fib1 fib2 значения двух первых элементов ряда
+-> В цикле выполнять движение по ряду фибоначи fibSum = fib1 + fib2, fib1 = fib2, fib2 = fibSum
+-> цикл продолжить до n - 2, поскольку первые 2 члена последовательности мы определили
+-> написать условие (если n 1 или 2, то вернуть соответствующий член последовательности)
+*/
+function nthFibo(n) {
+  if(n === 1) return 0
+  if(n === 2) return 1
+  let fib1 = 0
+  let fib2 = 1
+  let fibSum
+  let i = 0
+  while (i < n - 2) {
+      fibSum = fib1 + fib2
+      fib1 = fib2
+      fib2 = fibSum
+      i++
+  }
+  return fibSum
+}
