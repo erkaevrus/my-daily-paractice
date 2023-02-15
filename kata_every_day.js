7498b4ac8e6506144397059d56989163afafdc14
erkaevrus <erk_08@mail.ru>
2023-02-15 12:51:34 +0500

    add task's solution 60

--- kata_every_day.js
+++ kata_every_day.js
@@ -1406,3 +1406,40 @@ function deepCount(a){
     }
     return count
 }
+
+
+/* 15.02.2023 #60 |6kyu|
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+You have to write a method, that return the length of the missing array.
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+If the array of arrays is null/nil or empty, the method should return 0.
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+I have created other katas. Have a look if you like coding and challenges.
+*/
+/*
+1. Понимание -> Дан массив массивов. Если отсортировать этот массив по длинам массивов, то мы увидим, что длины идут по возрастанию, кроме одной пропущенной длины. Нужно найти какой длины не хватает и вернуть ее значение.
+Если массив массивов пуст вернуть 0.
+Если подмассив пустой или нулевой, вернуть 0.
+Если массив содержит значение null вернуть 0.
+2. Планирование + Декомпозиция
+-> Преобразовать массив массивов в массив длин массивов и отсортировать их
+-> Пройтись по массиву в цикле и найти пропущенный элемент
+*/
+function getLengthOfMissingArray(arrayOfArrays) {
+    if (arrayOfArrays === null)  return 0
+    if (arrayOfArrays.includes(null)) return 0
+    if (arrayOfArrays.length === 0) return 0
+
+    let temp = arrayOfArrays.map(x => x.length).sort((a, b) => a - b)
+    if (temp.includes(0)) return 0
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] !== temp[i+1] - 1) {
+            return temp[i] + 1
+        }
+    }
+}
