dc6996401b0694ac37b498287453039d00f2690e
erkaevrus <erk_08@mail.ru>
2023-02-17 14:10:27 +0500

    add task's solution 62

--- kata_every_day.js
+++ kata_every_day.js
@@ -1472,3 +1472,17 @@ function numberOfPairs(gloves) {
     }
     return Object.values(obj).map(x => Math.floor(x/2)).reduce((acc, curr) => acc + curr)
 }
+
+
+
+/* 17.02.2023 #62 |7kyu|
+Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+min([1,2,3,4,5], 'value') // => 1
+min([1,2,3,4,5], 'index') // => 0
+*/
+function min(arr, toReturn) {
+    return toReturn === 'value' ?
+           Math.min(...arr) :
+           arr.indexOf(Math.min(...arr))
+  }
