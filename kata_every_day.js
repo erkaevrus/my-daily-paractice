c0c8c2dfbfabfd247d269ebb0eb3c270a1d1cb35
erkaevrus <erk_08@mail.ru>
2023-02-20 08:07:52 +0500

    add task's solution 65 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -1565,3 +1565,54 @@ function formatDuration (seconds) {
     }
    return 'now'
   }
+
+
+/*20.02.2023 #65 |CW 6kyu|
+You will be given a list of objects. Each object has type, material, and possibly secondMaterial. The existing materials are: paper, glass, organic, and plastic.
+Your job is to sort these objects across the 4 recycling bins according to their material (and secondMaterial if it's present), by listing the type's of objects that should go into those bins.
+Notes
+The bins should come in the same order as the materials listed above
+All bins should be listed in the output, even if some of them are empty
+If an object is made of two materials, its type should be listed in both of the respective bins
+The order of the type's in each bin should be the same as the order of their respective objects was in the input list
+Example
+input = [
+  {"type": "rotten apples", "material": "organic"},
+  {"type": "out of date yogurt", "material": "organic", "secondMaterial": "plastic"},
+  {"type": "wine bottle", "material": "glass", "secondMaterial": "paper"},
+  {"type": "amazon box", "material": "paper"},
+  {"type": "beer bottle", "material": "glass", "secondMaterial": "paper"}
+]
+
+output = [
+  ["wine bottle", "amazon box", "beer bottle"],
+  ["wine bottle", "beer bottle"],
+  ["rotten apples", "out of date yogurt"],
+  ["out of date yogurt"]
+]
+*/
+/*
+1. Понимание -> Вернуть Массив с 4мя массивами, в которые отсортированы предменты в соотсветствии с их материалом. Каждый массив отвечает за свой материал.
+2. Планирование + Декомпозиция
+-> Создать результирующий массив с массивами
+-> Перебрать в цикле объект и по условиям раcпределить типы по результирующему массиву
+*/
+function recycle(array) {
+    const bin = [[], [], [], []]
+
+    for (let obj of array) {
+        if (obj['material'] === 'paper' || obj['secondMaterial'] === 'paper') {
+            bin[0].push(obj['type'])
+        }
+        if (obj['material'] === 'glass' || obj['secondMaterial'] === 'glass') {
+            bin[1].push(obj['type'])
+        }
+        if (obj['material'] === 'organic' || obj['secondMaterial'] === 'organic') {
+            bin[2].push(obj['type'])
+        }
+        if (obj['material'] === 'plastic' || obj['secondMaterial'] === 'plastic') {
+            bin[3].push(obj['type'])
+        }
+    }
+    return bin
+}
