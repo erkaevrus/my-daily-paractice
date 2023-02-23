75b2ff51a1c2be3df1dd8a5755d371501bf6dfdc
erkaevrus <erk_08@mail.ru>
2023-02-23 20:43:39 +0500

    add task's solution 67 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -1637,3 +1637,34 @@ function unluckyDays(year) {
     }
     return count
 }
+
+
+/*23.02.2023 #67 |CW 6kyu|
+Run-length encoding (RLE) is a very simple form of data compression in which runs of data (that is, sequences in which the same data value occurs in many consecutive data elements) are stored as a single data value and count, rather than as the original run. Wikipedia
+Task
+Your task is to write such a run-length encoding. For a given string, return a list (or array) of pairs (or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct the original string by replicating the character sx ix times and concatening all those strings. Your run-length encoding should be minimal, ie. for all i the values si and si+1 should differ.
+Examples
+As the article states, RLE is a very simple form of data compression. It's only suitable for runs of data, as one can see in the following example:
+runLengthEncoding("hello world!")
+=>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+It's very effective if the same data value occurs in many consecutive data elements:
+runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+=> [[34,'a'], [3,'b']]
+*/
+/*
+1. Понимание -> Выполнить сжатие строки. Вернуть массив массивов, в  котором каждый подмассив кодируется символом и количеством повторений(только для символов следующих подряд)
+2. Планирование + Декомпозиция
+-> Создать результирующий массив
+-> Перебрать в цикле строку и по условиям проверки либо добавлять единичный массив вида [1, char], либо увеличивать счетчик на один, если следующий символ равняется предыдущему
+*/
+function runLengthEncoding(str){
+    const result = []
+    for (let i = 0; i < str.length; i++) {
+        if (result.length !== 0 && result[result.length - 1][1] === str[i]) {
+            result[result.length - 1][0]++
+        } else {
+            result.push([1, str[i]])
+        }
+    }
+    return result
+}
