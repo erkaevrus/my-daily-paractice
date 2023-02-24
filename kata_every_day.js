0958d567a8e3bfe5abd272b1fbebac28486fa884
erkaevrus <erk_08@mail.ru>
2023-02-24 11:45:55 +0500

    add task's solution 68 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -1668,3 +1668,32 @@ function runLengthEncoding(str){
     }
     return result
 }
+
+
+/*24.02.2023 #68 |CW 6kyu|
+Implement the method find which takes in an two parameters object and path. The path will be a period-delimited string of properties that we will use to traverse through our object to find our target value.
+Check out this Codecast to get started! https://codecast.qualified.io/interaction/594197b92f524d001c1ab790
+Edge Cases And Further Consideration
+Be sure to handle passing array indices. For example, if we have an object: { people: ['John', 'Dave', 'Lisa'] } and the path is 'people.1', the target value is 'Dave' which is the string at position 1 inside of the people array.
+Also this method should handle invalid paths. If we have an object { user: { name: 'Dan' } } and the path is 'user.wallet.money', we should return undefined. Valid paths are exclusive to properties on the object which are not inherited, in other words it is specific to this object and does not need to look up the prototype chain.
+*/
+/*
+1. Понимание -> написать функцию, которая принимает объект и путь(строка, с разделителем '.') и возвращает значение по этому пути либо undefined
+2. Планирование + Декомпозиция
+-> сделать из строки path массив по точкам
+-> в цикле пeребирать массив с путями
+-> проверять есть ли свойство у объекта и не наследуется ли свойство из прототипа
+-> если свойство существует переопределить объект и перейти к следующему свойству
+-> иначе вернуть undefined
+*/
+function find(object, path) {
+    let arrPath = path.split('.')
+    for (let elem of arrPath) {
+        if (object.hasOwnProperty(elem)) {
+            object = object[elem]
+        } else {
+            return undefined
+        }
+    }
+    return object
+}
