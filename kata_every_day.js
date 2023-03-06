13aa0a46de32e5575de0d72c043533c361a08d28
erkaevrus <erk_08@mail.ru>
2023-03-06 23:13:52 +0500

    add task's solution 78 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -1952,3 +1952,25 @@ Array.prototype.map = function(cb, context) {
     }
     return newArr
   }
+
+
+
+/*06.03.2023 #78 |6kyu|
+Given a Date (in JS and Ruby) or hours and minutes (in C and Python), return the angle between the two hands of a 12-hour analog clock in radians.
+Notes:
+The minute hand always points to the exact minute (there is no seconds hand).
+The hour hand does not "snap" to the tick marks: e.g. at 6:30 the angle is not 0 because the hour hand is already half way between 6 and 7.
+Return the smaller of the angles ( <= π ).
+Return π if the hands are opposite.
+Examples
+at noon the angle is: 0
+at 3:00 the angle is: π/2 (90 degrees)
+at 6:00 the angle is: π (180 degrees)
+at 9:00 the angle is: π/2 (90 degrees)
+*/
+function handAngle (date) {
+    const hours = date.getHours() % 12
+    const minutes = date.getMinutes()
+    const result = Math.abs((hours*30 + minutes*0.5) - (minutes*6))
+    return Math.min(((360 - result)*Math.PI)/180, (result*Math.PI)/180)
+}
