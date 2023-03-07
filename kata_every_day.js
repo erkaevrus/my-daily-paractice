56c561a17ec6a775a43985a356ea8148e8de0622
erkaevrus <erk_08@mail.ru>
2023-03-07 19:38:05 +0500

    add task's solution 79 - 7kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -1974,3 +1974,20 @@ function handAngle (date) {
     const result = Math.abs((hours*30 + minutes*0.5) - (minutes*6))
     return Math.min(((360 - result)*Math.PI)/180, (result*Math.PI)/180)
 }
+
+
+/*07.03.2023 #79 |7kyu|
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+Examples: (Input --> Output)
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+ */
+const number = function(array){
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      result.push(`${i+1}: ${array[i]}`)
+    }
+    return result
+}
