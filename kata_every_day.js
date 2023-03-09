8ff03150e68431b7565d60be369b95eac65811e3
erkaevrus <erk_08@mail.ru>
2023-03-09 18:20:15 +0500

    add task's solution 81 - 7kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2040,9 +2040,35 @@ Array.prototype.filter = function(cb, context) {
     const newArr = []
     let length = this.length
     for(let i = 0; i < length ; i++) {
-      if (i in this && cb.call(context, this[i], i, this)) {
-        newArr.push(this[i])
-      }
+        if (i in this && cb.call(context, this[i], i, this)) {
+            newArr.push(this[i])
+        }
     }
     return newArr
+  }
+
+/*09.03.2023 #81 |7kyu|
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+*/
+function openOrSenior(data){
+    const result = [];
+    for (let item of data) {
+      if (item[0] >= 55 && item[1] > 7) {
+        result.push('Senior')
+      } else {
+        result.push('Open')
+      }
+    }
+    return result;
   }
