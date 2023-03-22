b8a3bcb33b5ca0aac1f1000408ea19fae289bf04
erkaevrus <erk_08@mail.ru>
2023-03-22 10:55:55 +0500

    add task's solution 89 - 7kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2314,3 +2314,31 @@ function rowSumOddNumbers(n) {
   function rowSumOddNumbers(n) {
     return Math.pow(n, 3);
   }
+
+
+/*22.03.2023 #89 |7kyu|
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+Your function should only return a number, not the explanation about how you get that number.
+*/
+function getSum(a, b) {
+    if (a === b) return a
+
+    let count = 0
+
+    if (a < b) {
+        while (a <= b) { count += a++ }
+    } else {
+        while (a >= b) { count += a-- }
+    }
+    return count
+}
