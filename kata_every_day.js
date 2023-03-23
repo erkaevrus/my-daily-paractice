253528eeda95a21af1fb3e7e569badeb89e4d4c9
erkaevrus <erk_08@mail.ru>
2023-03-23 16:42:32 +0500

    add task's solution 90 - 8kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2342,3 +2342,13 @@ function getSum(a, b) {
     }
     return count
 }
+
+
+/*23.03.2023 #90 |8kyu|
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+function findAverage(array) {
+    return array.length === 0 ? 0 : array.reduce((acc, cur) => acc + cur) / array.length
+  }
