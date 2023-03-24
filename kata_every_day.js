fe77334cc0d7dd90dba16176a28fafcddd7bbf21
erkaevrus <erk_08@mail.ru>
2023-03-24 12:34:41 +0500

    add task's solution 92 - 8kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2404,3 +2404,16 @@ function towerBuilder(nFloors) {
     }
     return tower.reverse()
 }
+
+
+/*24.03.2023 #92 |8kyu|
+Your function takes two arguments:
+
+1. current father's age (years)
+2. current age of his son (years)
+
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - sonYearsOld * 2)
+  }
