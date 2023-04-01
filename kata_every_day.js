53c94020830bb8fc0ac77d8b3581d29aaec616f3
erkaevrus <erk_08@mail.ru>
2023-04-01 16:41:16 +0500

    add task's solution 95 - 7kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2498,3 +2498,15 @@ function getDivisorsCnt(n){
     }
     return count;
 }
+
+
+/*01.04.2023 #95 |7kyu|
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+function reverseWords(str) {
+  return str.split(' ').map(x => x.split('').reverse().join('')).join(' ')
+  }
