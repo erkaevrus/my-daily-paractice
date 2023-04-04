e6079f4da924faa34231684bc477b60bbb6f6846
erkaevrus <erk_08@mail.ru>
2023-04-04 22:04:30 +0500

    add task's solution 96 - 8kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2510,3 +2510,18 @@ Examples
 function reverseWords(str) {
   return str.split(' ').map(x => x.split('').reverse().join('')).join(' ')
   }
+
+
+/*04.04.2023 #96 |8kyu|
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo"
+name + " does not play banjo"
+Names given are always valid strings.
+*/
+function areYouPlayingBanjo(name) {
+return name[0].toUpperCase() !== 'R' ? `${name} does not play banjo` : `${name} plays banjo`
+}
