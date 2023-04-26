f86acf1ae2308619437cc056866f28ac72e5d57d
erkaevrus <erk_08@mail.ru>
2023-04-26 11:22:10 +0500

    add task's solution 103 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2746,4 +2746,37 @@ Array.prototype.size = function() {
     i++
   }
   return i
-};
+}
+
+
+/*26.04.2023 #103 |6kyu|
+This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+Explanation:
+
+square() must return a copy of the array, containing all values squared
+cube() must return a copy of the array, containing all values cubed
+average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+sum() must return the sum of all array values
+even() must return an array of all even numbers
+odd() must return an array of all odd numbers
+Note: the original array must not be changed in any case!
+
+Example
+var numbers = [1, 2, 3, 4, 5];
+
+numbers.square();  // must return [1, 4, 9, 16, 25]
+numbers.cube();    // must return [1, 8, 27, 64, 125]
+numbers.average(); // must return 3
+numbers.sum();     // must return 15
+numbers.even();    // must return [2, 4]
+numbers.odd();     // must return [1, 3, 5]
+ */
+Object.assign(Array.prototype, {
+  square() {return this.map(x => x**2)},
+  cube() {return this.map(x => x**3)},
+  average() {return this.reduce((curr, acc) => curr + acc, 0) / this.length},
+  sum() {return this.reduce((curr, acc) => curr + acc, 0)},
+  even() {return this.filter(x => x % 2 === 0)},
+  odd() {return this.filter(x => x % 2 !== 0)}
+})
