9f8800bd2369910de79e12884885c18e7352fe88
erkaevrus <erk_08@mail.ru>
2023-05-10 11:07:48 +0500

    add task's solution 104 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2780,3 +2780,28 @@ Object.assign(Array.prototype, {
   even() {return this.filter(x => x % 2 === 0)},
   odd() {return this.filter(x => x % 2 !== 0)}
 })
+
+
+/*10.05.2023 #104 |6kyu|
+Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+function expandedForm(num) {
+  const strNum = num.toString()
+  let res = ''
+  if (strNum.length === 1) return strNum
+  for (let i = 0; i < strNum.length; i++) {
+    if (strNum[i] === '0') continue
+    if (strNum.length - 1 === i) {
+      res += ` + ${strNum[i]}`
+      break
+    }
+    res += ` + ${num.toString()[i]}${'0'.repeat(strNum.length - (i + 1))}`
+  }
+  return res.slice(2).trim()
+}
