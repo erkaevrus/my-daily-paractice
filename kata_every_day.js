a35831320289bbf2ef9d7aac3b5566d5dc1eb804
erkaevrus <erk_08@mail.ru>
2023-05-16 12:40:13 +0500

    add task's solution 108 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -2897,3 +2897,64 @@ fives(myArray);               //returns [5, 10, 15];
 function factory(x){
   return array => array.map( a => a*x );
 }
+
+/*16.05.2023 #108 |6kyu|
+Introduction to getter and setter
+Javascript classes can declare getter and setters using the following format:
+
+class Engine {
+  constructor(watts) {
+    this.watts = watts;
+  }
+  get horsepower() {
+    return this.watts / 745.7;
+  }
+  set horsepower(hp) {
+    this.watts = 745.7 * hp;
+  }
+}
+and be used as so:
+
+let bossV8 = new Engine(279637.5);
+console.log(bossV8.horsepower); // => 375
+bossV8.horsepower = 385;
+console.log(bossV8.watts); // => 287094.5
+The problem
+There is a preloaded class of Person and it needs a new getter and setter. Person is defined with a constructor that takes a first name and a last name, and provides a mean to get the first name, last name, and full name. Bellow is the preloaded class:
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+}
+and can be used as so:
+
+let marcusFenix = new Person('Marcus', 'Fenix');
+console.log(marcusFenix.firstName); // => 'Marcus'
+console.log(marcusFenix.lastName); // => 'Fenix'
+console.log(marcusFenix.getName()); // => 'Marcus Fenix'
+Add a getter of name that returns the full name
+Add a setter of name that modifies first name and last name
+After adding the new getter and setter, a Person can be used as so:
+
+let augustusCole = new Person('Augustus', 'Cole');
+augustusCole.name = 'Cole Train';
+console.log(augustusCole.firstName); // => 'Cole'
+console.log(augustusCole.lastName); // => 'Train'
+console.log(augustusCole.getName()); // => 'Cole Train'
+console.log(augustusCole.name); // => 'Cole Train'
+*/
+Object.defineProperty(Person.prototype, 'name', {
+  get() {
+    return this.firstName + ' ' + this.lastName
+  },
+
+  set(value) {
+    [this.firstName, this.lastName] = value.split(' ')
+  }
+})
