4fd19445a44699e3358641ef79632fcf572bad3c
erkaevrus <erk_08@mail.ru>
2023-05-29 13:19:09 +0500

    add task's solution 110 - 6 kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -1069,6 +1069,7 @@ function getCard() {
   return bingo
 }
 
+
 function getRandom(min, max) {
 let rand = min + Math.random() * (max + 1 - min);
 return Math.floor(rand);
@@ -3038,3 +3039,18 @@ class PaginationHelper {
     return Math.floor(itemIndex / this.itemsPerPage)
 	}
 }
+
+
+/*29.05.2023 #110 |6kyu|
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+*/
+function createPhoneNumber(numbers){
+  return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`
+}
+
