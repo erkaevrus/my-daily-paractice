f95fc28e94a0fc4874b9e1966b248d58b828fca7
erkaevrus <erk_08@mail.ru>
2023-06-06 17:50:34 +0500

    add task's solution 117 - 4kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -3220,3 +3220,71 @@ function findHack(arr) {
   }
   return result
 }
+
+/*06.06.2023 #117 |4kyu|
+The purpose of this kata is to implement the undoRedo function.
+This function takes an object and returns an object that has these actions to be performed on the object passed as a parameter:
+set(key, value) Assigns the value to the key. If the key does not exist, creates it.
+get(key) Returns the value associated to the key.
+del(key) removes the key from the object.
+undo() Undo the last operation (set or del) on the object. Throws an exception if there is no operation to undo.
+redo() Redo the last undo operation (redo is only possible after an undo). Throws an exception if there is no operation to redo.
+After set() or del() are called, there is nothing to redo.
+All actions must affect to the object passed to undoRedo(object) function. So you can not work with a copy of the object.
+Any set/del after an undo should disallow new redos.
+*/
+function undoRedo(object) {
+  let lastActions = []
+  let undoneActions = []
+
+  return {
+    set(key, value) {
+      if (object.hasOwnProperty(key) === true) {
+        lastActions.push(['edit', key, object[key], value])
+      } else {
+        lastActions.push(['set', key, value])
+      }
+      object[key] = value
+      undoneActions = []
+    },
+
+    get(key) {
+      return object[key]
+    },
+
+    del(key) {
+      if (object[key]) {
+        lastActions.push(['del', key, object[key]])
+        delete object[key]
+        undoneActions = []
+      }
+    },
+
+    undo() {
+      if (lastActions.length === 0) {
+        throw new  Error('No actions')
+      }
+
+      const popped = lastActions.pop()
+      if (popped[0] === 'set') delete object[popped[1]]
+      if (popped[0] === 'edit') object[popped[1]] =  popped[2]
+      if (popped[0] === 'del') object[popped[1]] = popped[2]
+      
+      undoneActions.push(popped)
+    },
+
+    redo() {
+      if (undoneActions.length === 0) {
+        throw new Error('No undone actions')
+      }
+
+      const popped = undoneActions.pop()
+      
+      if (popped[0] === 'set') object[popped[1]] = popped[2]
+      if (popped[0] === 'edit') object[popped[1]] = popped[3]
+      if (popped[0] === 'del') delete object[popped[1]]
+    
+      lastActions.push(popped)
+    }
+  }
+}
