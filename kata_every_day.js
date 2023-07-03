a9880109d35b8cc9ae5698605586d9098a6052e7
erkaevrus <erk_08@mail.ru>
2023-07-03 12:23:59 +0400

    add task's solution 124 - 6kyu

--- kata_every_day.js
+++ kata_every_day.js
@@ -3583,3 +3583,36 @@ class File {
     return this.contents[this.index++]
   }
 }
+
+/*03.07.2023 #124 |6kyu|
+You are developing an image hosting website.
+
+You have to create a function for generating random and unique image filenames.
+
+Create a function for generating a random 6 character string which will be used to access the photo URL.
+
+To make sure the name is not already in use, you are given access to an PhotoManager object.
+
+You can call it like so to make sure the name is unique
+
+// at this point, the website has only one photo, hosted on the 'ABCDEF' url
+photoManager.nameExists('ABCDEF'); // returns true
+photoManager.nameExists('BBCDEF'); // returns false
+Note: We consider two names with same letters but different cases to be unique.
+ */
+function generateName() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const res = []
+  while (res.length <= 6) {
+    let randomIndex = Math.floor(Math.random()*25)
+    res.push(alphabet[randomIndex])
+    
+    if (res.length === 6) {
+      if (photoManager.nameExists(res)) {
+        res.length = 0
+      } else {
+        return res.join('')
+      }
+    }
+  }
+}
